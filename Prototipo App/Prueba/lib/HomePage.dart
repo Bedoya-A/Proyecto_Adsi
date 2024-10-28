f033d65ef7232a8098127247d602b115edccc158
Bedoya-A <josealejandrobedoyacabezas@gmail.com>
2024-10-28 13:44:14 -0500

    Eliminar flecha atrás en HomePage

--- Prototipo App/Prueba/lib/HomePage.dart
+++ Prototipo App/Prueba/lib/HomePage.dart
@@ -10,6 +10,7 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        automaticallyImplyLeading: false,
         title: Row(
           children: [
             GestureDetector(
